1b83b2c0314ba13306e4bae1c4d46c301407da85
vishalpatil5579 <135108757+vishalpatil5579@users.noreply.github.com>
2025-04-07 20:04:03 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello from Node.js app! 🚀'));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
